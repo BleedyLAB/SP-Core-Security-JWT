b8232a771d37110793d384af24aef21fb805bb81
Alexander Lazovski <Lazovski1991@gmail.com>
2020-11-22 18:21:04 +0300

    "refactoring 2"

--- src/main/java/ru/studentsplatform/security/model/User.java
+++ src/main/java/ru/studentsplatform/security/model/User.java
@@ -2,47 +2,40 @@ package ru.studentsplatform.security.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(name = "name")
-	private String name;
-	@Column(name = "email")
-	private String email;
-	@Column(name = "password")
-	private String password;
-	@Column(name = "role")
-	@Enumerated(EnumType.STRING)
-	private Role role;
-	@Column(name = "registered")
-	private LocalDateTime localDateTime;
-	@Column(name = "status")
-	private boolean status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(name = "registered")
+    private LocalDateTime localDateTime;
+    @Column(name = "status")
+    private boolean status;
 
-	@Override
-	public String toString() {
-		return "User{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", email='" + email + '\'' +
-				", password='" + password + '\'' +
-				", role=" + role +
-				", localDateTime=" + localDateTime +
-				", status=" + status +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", localDateTime=" + localDateTime +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/ru/studentsplatform/security/service/UserDetailServiceImpl.java
+++ src/main/java/ru/studentsplatform/security/service/UserDetailServiceImpl.java
@@ -20,44 +20,39 @@ import java.util.Optional;
 @Service
 public class UserDetailServiceImpl implements UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	@Autowired
-	public UserDetailServiceImpl(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+    @Autowired
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(email).orElseThrow(
-				() -> new UsernameNotFoundException("User doesn't exists"));
-		return SecurityUser.fromUser(user);
-	}
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User doesn't exists"));
+        return SecurityUser.fromUser(user);
+    }
 
-	@Override
-	public User findUserById(Long userId) {
-		Optional<User> userFromDb = userRepository.findById(userId);
-		return userFromDb.orElse(new User());
-	}
+    @Override
+    public User findUserById(Long userId) {
+        Optional<User> userFromDb = userRepository.findById(userId);
+        return userFromDb.orElse(new User());
+    }
 
-	@Override
-	public List<User> findAllUsers() {
-		return userRepository.findAll();
-	}
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
 
-	@Override
-	public boolean saveUser(User user) {
-		/*Optional<User> userFromDb = userRepository.findByEmail(user.getEmail());
-
-		if (userFromDb.isPresent()) {
-			return false;
-		}*/
-		user.setRole(Role.USER);
-		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-		user.setStatus(true);
-		user.setLocalDateTime(LocalDateTime.now());
-		System.out.println(user);
-		userRepository.save(user);
-		return true;
-	}
+    @Override
+    public boolean saveUser(User user) {
+        user.setRole(Role.USER);
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        user.setStatus(true);
+        user.setLocalDateTime(LocalDateTime.now());
+        System.out.println(user);
+        userRepository.save(user);
+        return true;
+    }
 }
